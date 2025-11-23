191ecab6db9f08b9e5eaadd879ddcb6f1a54ae69
omodele temitope <omodeletemitope12@gmail.com>
2025-11-23 17:51:36 -0600

    commited

--- src/Subpages/Biooriginal.jsx
+++ src/Subpages/Biooriginal.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import { Link } from 'react-router-dom'
-import img1 from '../assets/Sneakers.jpg';
-import img2 from '../assets/Sneakers2.jpg';
+import img1 from '../assets/sneakers.jpg'
+import img2 from '../assets/sneakers.jpg';
 import ProductDetails from '../Components/ProductDetails'
 
 const Biooriginal = () => {
